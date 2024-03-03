69ac9200b60e4505204bc32a142e2de0b77047d4
J3SSY-ANDU <jessy.andujar@upr.edu>
2024-03-02 23:45:47 -0400

    navbar updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 export function Navbar() {
     return (
         <div>
-            <Link to={'/'}>Home</Link>
+            <Link to={'/'}>Logo</Link>
             <Link to={'/about'}>About</Link>
             <Link to={'/explore'}>Explore</Link>
-            <Link to={'/become-artist'}>Become Artist</Link>
+            <Link to={'/trending'}>Trending🔥</Link>
             <Link to={'/FAQ'}>FAQ</Link>
         </div>
     )
