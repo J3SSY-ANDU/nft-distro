0c30011ec42e880eb21b086006a488e420642926
Jessy Andujar Cruz <124212160+J3SSY-ANDU@users.noreply.github.com>
2024-03-10 02:54:30 -0400

    explore section updated

--- src/containers/explore/Explore.jsx
+++ src/containers/explore/Explore.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './explore.css';
+import { Card } from '../../components';
+import { exploreList } from '..';
+import dropDown from '../../assets/dropDown.svg';
 
 export function Explore() {
     return (
-        <div className='app__explore'>
-            <h1>Explore</h1>
+        <div className='app__explore section__padding'>
+            <div className="app__explore-heading">
+                <h1>Explore</h1>
+                <div className='app__explore-heading_dropDown'>
+                    <p>Recently Added</p>
+                    <img src={dropDown} alt='drop-down arrow'/>
+                </div>
+            </div>
+            <div className='app__explore-container'>
+                {exploreList.map((card, index) => {
+                    return (
+                        <Card cardImage={card.img} cardTitle={card.title} avatarImage={card.avatarImage} avatarName={card.avatarName} key={card.avatarName + index}/>
+                    )
+                })}
+            </div>
         </div>
     )
 }
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,7 +1,18 @@
 import exboot1 from '../assets/exboot1.svg';
 import exboot2 from '../assets/exboot2.svg';
+import exboot3 from '../assets/exboot3.svg';
+import exboot4 from '../assets/exboot4.svg';
+import exboot5 from '../assets/exboot5.svg';
+import exboot6 from '../assets/exboot6.svg';
 import futureOfPolygonX from '../assets/futureOfPolygonX.svg';
+import futureOfPolygonX2 from '../assets/futureOfPolygonX2.svg'
+import futureOfPolygonX3 from '../assets/futureOfPolygonX3.svg'
+import futureOfPolygonX4 from '../assets/futureOfPolygonX4.svg'
 import blueWave2 from '../assets/blueWave2.svg';
+import blueWave1 from '../assets/blueWave1.svg';
+import blueWave3 from '../assets/blueWave3.svg';
+import blueWave4 from '../assets/blueWave4.svg';
+import blueWave5 from '../assets/blueWave5.svg';
 import perperzonAvatar from '../assets/perperzon-avatar.svg';
 import richardAvatar from '../assets/richard-avatar.svg';
 import ranking1 from '../assets/ranking1.svg';
@@ -9,6 +20,8 @@ import ranking2 from '../assets/ranking2.svg';
 import ranking3 from '../assets/ranking3.svg';
 import ranking4 from '../assets/ranking4.svg';
 import ranking5 from '../assets/ranking5.svg';
+
+
 export { Header } from './header/Header';
 export { Trending } from './trending/Trending';
 export { Leaderboard } from './leaderboard/Leaderboard';
@@ -89,4 +102,79 @@ export const rankingList = [
         avatarName: "Antonson",
         coinAmount: "3.421"
     }
+]
+
+export const exploreList = [
+    {
+        img: exboot1,
+        title: "ExBoot #1",
+        avatarImage: perperzonAvatar,
+        avatarName: "Perperzon",
+    },
+    {
+        img: exboot3,
+        title: "ExBoot #3",
+        avatarImage: perperzonAvatar,
+        avatarName: "Perperzon",
+    },
+    {
+        img: futureOfPolygonX2,
+        title: "Future of Polygon X #2",
+        avatarImage: richardAvatar,
+        avatarName: "Richard",
+    },
+    {
+        img: blueWave1,
+        title: "Blue Wave #1",
+        avatarImage: perperzonAvatar,
+        avatarName: "Anderson",
+    },
+    {
+        img: exboot4,
+        title: "ExBoot #4",
+        avatarImage: perperzonAvatar,
+        avatarName: "Perperzon",
+    },
+    {
+        img: exboot5,
+        title: "ExBoot #5",
+        avatarImage: perperzonAvatar,
+        avatarName: "Perperzon",
+    },
+    {
+        img: futureOfPolygonX3,
+        title: "Future of Polygon #3",
+        avatarImage: richardAvatar,
+        avatarName: "Richard",
+    },
+    {
+        img: blueWave3,
+        title: "Blue Wave #3",
+        avatarImage: richardAvatar,
+        avatarName: "Anderson",
+    },
+    {
+        img: exboot6,
+        title: "ExBoot #6",
+        avatarImage: perperzonAvatar,
+        avatarName: "Perperzon",
+    },
+    {
+        img: exboot6,
+        title: "Figure #2",
+        avatarImage: perperzonAvatar,
+        avatarName: "Michael",
+    },
+    {
+        img: futureOfPolygonX4,
+        title: "Future of Polygon #4",
+        avatarImage: richardAvatar,
+        avatarName: "Richard",
+    },
+    {
+        img: blueWave5,
+        title: "Blue Wave #5",
+        avatarImage: richardAvatar,
+        avatarName: "Anderson",
+    },
 ]
