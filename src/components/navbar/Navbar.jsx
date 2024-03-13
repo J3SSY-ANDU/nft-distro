2aa15273da48a26a1b2e9445cf222edf939e2f01
J3SSY-ANDU <jessy.andujar@upr.edu>
2024-03-13 01:00:56 -0400

    navbar responsive design for screens of 500px below

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './navbar.css';
 // import { Link } from "react-router-dom";
 import logoFigure1 from '../../assets/logo/Polygon 1.svg';
@@ -6,12 +6,16 @@ import logoFigure2 from '../../assets/logo/Polygon 2.svg';
 import logoFigure3 from '../../assets/logo/Polygon 3.svg';
 import searchIcon from '../../assets/ri_search-line.svg';
 import backgroundImage from '../../assets/header-background.svg';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 export function Navbar() {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
     return (
         <div className="app__navbar section__padding" style={{background: `url(${backgroundImage})`}}>
             <div className="app__navbar-left">
-                <a className="app__navbar-left_logo" to={'/'}>
+                <a className="app__navbar-left_logo" href="#">
                     <img src={logoFigure1} alt="logo Figure 1"/>
                     <img src={logoFigure2} alt="logo figure 2"/>
                     <img src={logoFigure3} alt="logo figure 3"/>
@@ -22,9 +26,19 @@ export function Navbar() {
                 </div>
             </div>
             <div className="app__navbar-links">
-                <a to={'/explore'} className="link">Explore</a>
-                <a to={'/explore'} className="link">Trending 🔥</a>
-                <a to={'/FAQ'} className="link">FAQ</a>
+                <a href="explore" className="link">Explore</a>
+                <a href="trending" className="link">Trending 🔥</a>
+                <a href="/" className="link">FAQ</a>
+            </div>
+            <div className="app__navbar-menu">
+                {!toggleMenu ? <MenuIcon fontSize="small"  onClick={() => setToggleMenu(true)}/> : 
+                    <div className="app__navbar-menu_container">
+                        <CloseIcon className="close-icon" fontSize="small" onClick={() => setToggleMenu(false)}/>
+                        <a href="explore" className="link">Explore</a>
+                        <a href="trending" className="link">Trending 🔥</a>
+                        <a href="/" className="link">FAQ</a>
+                    </div>
+                }
             </div>
         </div>
     )
