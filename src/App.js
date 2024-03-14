73752407e135323052ae1c9b12d606460caa681e
J3SSY-ANDU <jessy.andujar@upr.edu>
2024-03-14 17:49:03 -0400

    loader created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home, About, Explore, FAQ, BecomeArtist } from './pages';
-import Layout from './Layout';
+// import Layout from './Layout';
+import Loader from './components/loader/Loader';
 
 function App() {
+  const [loader, setLoader] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoader(false);
+        }, 3000)
+    }, [])
+
   return (
-    <Home />
+      loader ? <Loader /> : <Home />
   );
 }
 
---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import { Box, Grow, LinearProgress } from '@mui/material'
+import figure1 from '../../assets/logo/Polygon 1.svg';
+import figure2 from '../../assets/logo/Polygon 2.svg';
+import figure3 from '../../assets/logo/Polygon 3.svg';
+
+const Loader = () => {
+    const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setProgress((prevProgress) => {
+                const newProgress = prevProgress + 10;
+                return newProgress >= 100 ? 100 : newProgress;
+            });
+        }, 250);
+
+        return () => {
+            clearInterval(timer);
+        };
+    })
+
+    return (
+        <Box display={'flex'} justifyContent={'center'} alignItems={'center'} height={'100vh'} className='app__loader'>
+            <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} gap={'4rem'} width={'40%'} className='app__loader-container'>
+                <Box display={'flex'} justifyContent={'center'} gap={'0.3rem'}>
+                    <Grow in={true} timeout={{enter: 2000}} style={{transformOrigin: 'center', transitionDelay: '200ms'}}>
+                        <img width={'30px'} src={figure1} alt='logo figure'/>
+                    </Grow>
+                    <Grow in={true} timeout={{enter: 2000}} style={{transformOrigin: 'center', transitionDelay: '600ms'}}>
+                        <img width={'30px'} src={figure2} alt='logo figure'/>
+                    </Grow>
+                    <Grow in={true} timeout={{enter: 2000}} style={{transformOrigin: 'center', transitionDelay: '1000ms'}}>
+                        <img width={'30px'} src={figure3} alt='logo figure'/>
+                    </Grow>
+                </Box>
+                <LinearProgress variant='determinate' value={progress}/>
+            </Box>
+        </Box>
+  )
+}
+
+export default Loader;
